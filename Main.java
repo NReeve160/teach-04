93687674088d4f0dd3682e3d103e4f78a12d5105
Nathan Reeve <44732071+NReeve160@users.noreply.github.com>
2021-05-21 20:46:38 -0600

    Add file

---
+++ Main.java
@@ -0,0 +1,12 @@
+package reeve;
+
+public class HelloFrom {
+
+    static void helloFrom() {
+        System.out.println("Hello From Nathan Reeve");
+    }
+
+    public static void main(String[] args) {
+        helloFrom();
+    }
+}
